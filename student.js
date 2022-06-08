07b5d0b78ba9fce435ce3ddd115b8cd812e9e7d5
Eduardo Olivares <107024703+eduardo-olivares-cofc@users.noreply.github.com>
2022-06-07 21:05:51 -0700

    lesson 2

--- student.js
+++ student.js
@@ -1,3 +1,11 @@
+const name2 = "Eduardo Olivares";
+const a_message = "cs190 Javascript Lab 2";
+const code = "426";
+
+function displayinfo537() {
+    window.alert(name2 + " " + a_message + " " + code);
+}
+
 function confirmlink371(linkname) {
     if (confirm("Are you sure you want to jump to " + linkname + "?")) {
         location = linkname;
