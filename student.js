3de011017fc6aa7c61fc8635d7e36ac9b477236e
Eduardo Olivares <107024703+eduardo-olivares-cofc@users.noreply.github.com>
2022-06-07 21:43:47 -0700

    Lab 3

--- student.js
+++ student.js
@@ -1,3 +1,25 @@
+// declare this variable outside of your functions
+special = 1;     // selects which item that is "on special"
+
+function displayspecial830() {
+
+    if (special == 1) {
+        alert("The current special is #1");
+    }
+    if (special == 2) {
+        alert("The current special is #2");
+    }
+    if (special == 3) {
+        alert("The current special is #3");
+    }
+    special = special + 1;
+    if (special > 3) {
+        special = 1;  // wrap around
+    }
+
+}
+
+
 var name2 = 'Eduardo Olivares';
 var a_message = 'cs190 Javascript Lab 2';
 var code = '426';
