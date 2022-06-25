c301d79cdb971beba33b479cd480c2a3a70ada21
Eduardo Olivares <eolivares2@my.canyons.edu>
2022-06-24 22:04:12 -0700

    2

---
+++ student2.js
@@ -0,0 +1,11 @@
+var companyname5 = "Drip Clothes Inc.";
+var address3 = "450 Main St. Valencia, CA 91344";
+var phonenumber5 = "(555) 515-1234";
+
+function getHeader(){
+    
+}
+
+function getFooter(){
+
+}
